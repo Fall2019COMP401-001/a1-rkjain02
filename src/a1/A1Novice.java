db7d853c89a25f1ff011c91335f322e7a54486de
RJ <RJ@theodora.wireless-1x.unc.edu>
2019-08-28 01:01:17 -0400

    Completed A1 novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,24 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int count = scan.nextInt();
 		
+		
+		
+		for(int i =0; i<count; i++) {
+			double sum = 0.00;
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numberOfItems = scan.nextInt();
+			for(int j =0; j<numberOfItems; j++) {
+				int quantityOfItem = scan.nextInt();
+				String nameOfItem = scan.next();
+				double priceOfItem = scan.nextDouble();
+				sum += quantityOfItem * priceOfItem;
+			}
+			
+			System.out.println(firstName.charAt(0) + ". " + lastName + ": " + sum);
+	
+		}
 	}
 }
